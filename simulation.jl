2975177988b784032d77facb8f578212896801d0
Mijail Todorovich <mijailtodorovich+git@gmail.com>
2019-05-22 15:24:18 -0300

    Delete old sim!, step! methods

--- simulation.jl
+++ simulation.jl
@@ -1,21 +1,5 @@
 """
 Update position, velocity and force of particles affected by a
-force 'forcefield(particle)' after a time interval 'dt'
-"""
-function step!(
-    particle::Particle,
-    forcefield,
-    dt::Real)
-
-  particle.position += particle.velocity*dt
-  particle.velocity += (particle.force/particle.mass)*dt
-  particle.force = forcefield(particle)
-
-  return particle
-end
-
-"""
-Update position, velocity and force of particles affected by a
 force 'forcefield(particle[i], particle[j])' after a time interval 'dt'
 """
 function step!(
@@ -36,61 +20,6 @@ function step!(
   end
 
   return particles
-end
-
-"""
-Run a simulation of a single particle and save it to arr
-"""
-function sim!(
-    particle::Particle{N,T},
-    forcefield,
-    arr::AbstractVector{Particle{N,T}}=Array{Particle{N,T},1}(),
-    dt::Real=0.1,
-    nsteps::Integer=1000) where {N, T<:AbstractFloat}
-
-  particle.force = forcefield(particle)
-  push!(arr, deepcopy(particle))
-
-  for i in 1:nsteps
-    step!(particle, forcefield, dt)
-    push!(arr, deepcopy(particle))
-  end
-
-  return arr
-end
-
-"""
-Run a simulation and save it to mat
-"""
-function sim!(
-    particles::AbstractVector{Particle{N,T}},
-    forcefield,
-    mat::Matrix{Particle{N,T}}=Matrix{Particle{N,T}}(),
-    dt::Real=0.1,
-    nsteps::Integer=1000) where {N, T<:AbstractFloat}
-
-  for particle in particles
-    particle.force = zeros(SVector{N,T})
-  end
-
-  for i=1:size(particles), j=i+1:size(particles)
-    forces = forcefield(particles[i], particles[j])
-    particles[i].force += forces[1]
-    particles[j].force += forces[2]
-  end
-
-  if size(mat) == (0,0)
-    mat = deepcopy(particles)
-  else
-    mat = vcat(mat, deepcopy(particles))
-  end
-
-  for i in 1:nsteps
-    step!(particles, forcefield, dt)
-    mat = vcat(mat, deepcopy(particles))
-  end
-
-  return mat
 end
 
 function sim(
