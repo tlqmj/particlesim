3572775d29080cf8a542ba793358fac7216262d4
Mijail Todorovich <mijailtodorovich+git@gmail.com>
2019-05-23 17:00:42 -0300

    Set G as a constant

--- forcefields.jl
+++ forcefields.jl
@@ -1,3 +1,5 @@
+const G = 4.302e-3 # G is in pc*M⊙⁻¹ (km/s)²
+
 """
 Returns tuple of forces felt by p₁ and p₂ due to their gravitational interaction
 """
@@ -6,7 +8,7 @@ function gravitationalattraction(
     p₂::Particle{N}) where {N}
 
   d₂₁ = p₂.position - p₁.position
-  f₁  = 6.67408E-11*p₁.mass*p₂.mass*d₂₁/dot(d₂₁,d₂₁)^(3/2)
+  f₁  = G*p₁.mass*p₂.mass*d₂₁/dot(d₂₁,d₂₁)^(3/2) # G is in pc*M⊙⁻¹ (km/s)²
 
   return f₁, -f₁
 end
